2a8280828a0248f1cde1d30e40aa99a5b420be5b
lmnguyet <lminhnguyet@lminhnguyet.tma.com.vn>
2025-03-28 17:09:39 +0700

    seventh commit

--- spark/stream.py
+++ spark/stream.py
@@ -1,62 +1,132 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+import time
+from delta.tables import DeltaTable
 
-def read_stream():
-    pass
-
-def create_bucket():
-    pass
-
-def write_bucket():
-    pass
-
-def main():
-    # config spark session
-    spark = SparkSession.builder \
+# create spark session
+spark = SparkSession.builder \
         .appName("SpakApp") \
-        .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore")\
-        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")\
         .getOrCreate()
-        
-    # read_stream
+
+delta_path = "s3a://test2/films"
+
+# read stream from kafka
+def read_stream():
     df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "kafka:9092") \
         .option("subscribe", "dbserver1.pixar_films.films") \
         .option("startingOffsets", "earliest") \
+        .option("failOnDataLoss", "false") \
         .load()
+    return df
 
-    df.printSchema()
+# process stream df
+def process_stream(df):
+    schema = StructType([
+        StructField("payload", StructType([
+            StructField("before", StructType([
+                StructField("number", IntegerType(), nullable=False)
+            ]))
+            , StructField("after", StructType([
+                StructField("number", IntegerType(), nullable=False),
+                StructField("film", StringType(), nullable=False),
+                StructField("release_date", DateType(), nullable=True),
+                StructField("run_time", IntegerType(), nullable=True),
+                StructField("film_rating", StringType(), nullable=True),
+                StructField("plot", StringType(), nullable=True)
+            ]))
+            , StructField("op", StringType(), nullable=False)
+            # , StructField("ts_ms", IntegerType(), nullable=False)
+        ]))
+    ])
 
-    df = df.selectExpr("CAST(value as STRING) as value_str", "value")
-    # df = df.select(col("key").cast("string"),col("value").cast("string"),"topic","partition","offset","timestamp","timestampType")
+    processed_df = df.select(from_json(col("value").cast("string"), schema).alias("parsed_value"))
 
-    # query = df.writeStream \
-    # .outputMode("append") \
-    # .format("memory") \
-    # .queryName("kafka_results") \
-    # .start()
+    # window_spec = Window.partitionBy(
+    #     when(col("parsed_value.payload.op") == "d", col("parsed_value.payload.before.number"))\
+    #     .otherwise(col("parsed_value.payload.after.number"))
+    # ).orderBy(desc("parsed_value.payload.ts_ms"))
 
-    # import time
-    # time.sleep(90)
+    # processed_df = parsed_df.withColumn("row_num", row_number().over(window_spec)) \
+    #     .filter("row_num = 1") \
+    #     .drop("row_num")
+    
+    upsert_df = processed_df.select(\
+            when(col("parsed_value.payload.op") == "d", col("parsed_value.payload.before.number")).otherwise(col("parsed_value.payload.after.number")), \
+            col("parsed_value.payload.after.film"),\
+            col("parsed_value.payload.after.release_date").cast("date"),\
+            col("parsed_value.payload.after.run_time"),\
+            col("parsed_value.payload.after.film_rating"),\
+            col("parsed_value.payload.after.plot"),\
+            col("parsed_value.payload.op")
+        )
+    # , col("parsed_value.payload.op"))
+    return upsert_df
 
-    # print("printing df...")
-    # spark.sql("SELECT * FROM kafka_results").show(truncate=False)
+# create bucket and delta table if not exist
+def create_bucket():
+    pass
 
-    # query = df.writeStream \
-    # .format("delta") \
-    # .option("path", "s3a://test/films") \
-    # .option("checkpointLocation", "s3a://test/checkpoints") \
-    # .start()
+# write stream into delta table
+def write_bucket():
+    pass
 
-    query = df.writeStream \
-    .format("delta") \
-    .outputMode("append") \
-    .option("path", "s3a://test/films") \
-    .option("checkpointLocation", "s3a://test/checkpoints").start()
+# refresh query data from delta table
+def refresh():
+    while True:
+        if DeltaTable.isDeltaTable(spark, delta_path):
+            df = spark.read.format("delta").load(delta_path)
+            print(f"Tổng số bản ghi: {df.count()}")
+            df.show(40)
+        else:
+            print(f"Delta Table tại {delta_path} chưa tồn tại!")
+        time.sleep(30)
 
-    df1 = spark.read.format("delta").load("s3a://test/films")
-    print(df1)
+def upsertToDelta(microBatchOutputDF, batchId):
+    if not DeltaTable.isDeltaTable(spark, delta_path):
+        print("Creating delta table ...")
+        microBatchOutputDF.limit(0).write.format("delta").save(delta_path)
+
+    delta_table = DeltaTable.forPath(spark, delta_path)
+    
+    (delta_table.alias("t").merge(
+        microBatchOutputDF.alias("s"),
+        "s.number = t.number")
+        .whenMatchedUpdateAll()
+        .whenNotMatchedInsertAll()
+        .execute()
+        # .whenMatchedDelete("s.op = 'd'")
+    )
+
+def main():
+        
+    # read_stream
+    df = read_stream()
+
+    processed_df = process_stream(df)
+
+    # spark.sql(f"""
+    # CREATE TABLE IF NOT EXISTS delta.`s3a://test1/films` (
+    #     number INT,
+    #     film STRING,
+    #     release_date DATE,
+    #     run_time INT,
+    #     film_rating STRING,
+    #     plot STRING
+    # ) USING DELTA
+    # """)
+    # delta_table = DeltaTable.forPath(spark, delta_path)
+
+    query = processed_df.writeStream\
+    .foreachBatch(upsertToDelta)\
+    .option("checkpointLocation", "s3a://test2/checkpoints")\
+    .outputMode("update")\
+    .start()
+
+    refresh()
 
     query.awaitTermination()
 
@@ -65,6 +135,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # spark.sql("select * from delta.`s3a://test1/films`").show(40)
+    # test()
+
+
+
         # "org.apache.hadoop:hadoop-aws:3.3.0") \
 # .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
 # .config("spark.hadoop.fs.s3a.access.key", "root") \
